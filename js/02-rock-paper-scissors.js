0ef5c63a2a043acfe36e0893c36325988e9d1d71
dlabrat <107654952+dlabrat@users.noreply.github.com>
2024-10-15 16:45:22 -0700

    almost complete

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -3,7 +3,7 @@ let usrChoice
 let usrNum, coNum 
 
 let randomNum = Math.floor(Math.random()*3)+1
-usrChoice = parseInt(prompt("Enter choice of 1. rock, 2. paper or 3. scissors.\n Enter number:  "))
+usrChoice = parseInt(prompt("Enter choice of 1. rock, 2. paper or 3. scissors.\nEnter number:  "))
 switch (randomNum) {
     case 1: coNum = 'rock'
     break
@@ -21,28 +21,28 @@ switch (usrChoice) {
     break
 }
 
-console.log('You choose '+ usrNum +' and computer chose '+ coNum +'.')
+alert('You choose '+ usrNum +' and computer chose '+ coNum +'.')
 if (usrNum === coNum) {
-    console.log('We are tied!')
+    alert('We are tied!')
 }
 else if (usrNum == 'rock' && coNum =='paper') {          // paper beats rock
-    console.log('Computer wins!')
+    alert('Computer wins!')
 } 
 else if (usrNum == 'rock' && coNum =='scissors') {        // rock beats scissors
-    console.log('You win!')
+    alert('You win!')
 } 
 else if (usrNum == 'paper' && coNum =='rock') {          // paper beats rock
-    console.log('You win!')
+    alert('You win!')
 } 
 else if (usrNum == 'paper' && coNum =='scissors') {        // scissors beat paper
-    console.log('Computer wins!')
+    alert('Computer wins!')
 } 
 else if (usrNum == 'scissors' && coNum =='rock') {          // rock beats scissors
-    console.log('Computer wins!')
+    alert('Computer wins!')
 } 
 else if (usrNum == 'scissors' && coNum =='paper') {        // scissors beat paper
-    console.log('You win!')
+    alert('You win!')
 } 
 else {
-    console.log('Sorry, bad input.')
+    alert('Sorry, bad input.')
 }
--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,9 +1,43 @@
-// ADD A FUNCTION CALLED CALCULATE
+// The Basic Calculator 
+let givnNum, givnNum2, numResult
+let usrOpt
 
+// ADD A FUNCTION CALLED CALCULATE
+function calcNumbs(myNum, myNum2, chsnOpt) {
+    let myResult
+    console.log(myNum)
+    console.log(myNum2)
+    switch(chsnOpt) {
+        case 1: 
+        myResult = myNum+myNum2
+        return('The added numbers give '+ myResult+'.')
+        break
+        case 2: 
+        myResult = myNum-myNum2
+        return('The subtraction gives '+ myResult+'.')
+        break
+        case 3: 
+        myResult = myNum*myNum2
+        return('The multiplication gives '+ myResult+'.')
+        break
+        case 4: 
+        myResult = myNum/myNum2
+        return('The division gives '+ myResult.toFixed(3)+'.')
+        break
+    }
+   
+}
 // COLLECT FIRST NUMBER FROM USER
+givnNum = parseInt(prompt("Enter first number for calculation:  "))
 
 // COLLECT SECOND NUMBER FROM USER
+givnNum2 = parseInt(prompt("Enter second number for calculation:  ")) 
 
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+usrOpt = parseInt(prompt("Enter choice of operation \n1. + \n2. - \n3. * \n4. / \nEnter number:"))
+console.log(usrOpt)
 
 // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+numResult= calcNumbs(givnNum,givnNum2,usrOpt)
+alert(numResult)
+
